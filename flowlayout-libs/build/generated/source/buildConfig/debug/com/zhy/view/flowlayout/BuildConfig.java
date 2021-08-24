1ff17358ae4ff6cd73b2972253395c8dc9bf5f52
小曹 <1572651596@qq.com>
2021-08-24 19:05:48 +0800

    底部按钮刷新

---
+++ flowlayout-libs/build/generated/source/buildConfig/debug/com/zhy/view/flowlayout/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zhy.view.flowlayout;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.zhy.view.flowlayout";
+  public static final String BUILD_TYPE = "debug";
+}
